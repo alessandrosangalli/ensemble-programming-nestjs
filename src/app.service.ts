e16d53abc2e4ddea944d12f6a2fb48c95b438a52
Alessandro Sangalli <sangalli.alessandro@gmail.com>
2022-03-27 20:13:42 -1000

    desfazendo teste

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,11 +5,4 @@ export class AppService {
   getHello(): string {
     return 'Hello, Everybody!';
   }
-
-  myMethod(): string {}
-
-  myTeammateMethod(): string {}
-
-  myMethod2(): string {}
-  // ok, we're done...
 }
